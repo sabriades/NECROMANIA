e6a401485c5d53f552654d4d660f8330bcc3998b
sabriades <sabrinaanna.desiderio@gmail.com>
2024-03-27 22:22:12 +0100

    INFINITE SCROLLING GROUND
    now I understand the logic behind it. it will be useful when it comes to the background

---
+++ Assets/SCRIPT/Scroller.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scroller : MonoBehaviour
+{
+    Material material;
+    Vector2 offset;
+
+    public int xVelocity, yVelocity;
+
+    private void Awake()
+    {
+        material = GetComponent<Renderer>().material;
+    }
+
+
+    void Start()
+    {
+        offset = new Vector2(xVelocity, yVelocity);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        material.mainTextureOffset += offset * Time.deltaTime;
+
+    }
+}
