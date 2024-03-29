389f07ceeeec7ffc75add131111bd693d14b8a21
sabriades <sabrinaanna.desiderio@gmail.com>
2024-03-29 11:06:46 +0100

    ROTATING BLADE
    added obstacle

---
+++ Assets/Blades.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blades : MonoBehaviour
+{
+
+    public float rotatespeed; 
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 0, rotatespeed);
+        
+    }
+}
