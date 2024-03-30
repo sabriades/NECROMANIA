64d51c3eac9f6d844e520a409e2c2770363c3338
sabriades <sabrinaanna.desiderio@gmail.com>
2024-03-30 10:24:42 +0100

    NOW PLAYER JUMPS!
    all the frames in the animations are successfully imported

--- Assets/PlayerMoviment.cs
+++ Assets/PlayerMoviment.cs
@@ -1,12 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
-
 using UnityEngine;
 
 public class PlayerMoviment : MonoBehaviour
 {
-
     [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Animator animator; // Aggiunto Animator
     [SerializeField] private float jumpForce = 10f;
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private Transform feetPos;
@@ -17,28 +16,36 @@ public class PlayerMoviment : MonoBehaviour
     private bool isJumping = false;
     private float jumpTimer;
 
-
-    private void Update() {
+    private void Update()
+    {
         isGrounded = Physics2D.OverlapCircle(feetPos.position, groundDistance, groundLayer);
 
-        if (isGrounded && Input.GetButtonDown("Jump")) {
+        if (isGrounded && Input.GetButtonDown("Jump"))
+        {
             isJumping = true;
             rb.velocity = Vector2.up * jumpForce;
+            animator.SetBool("IsJumping", true); // Imposta il parametro booleano "IsJumping" su true
         }
 
-        if (isJumping && Input.GetButton("Jump")) {
-            if (jumpTimer < jumpTime) {
+        if (isJumping && Input.GetButton("Jump"))
+        {
+            if (jumpTimer < jumpTime)
+            {
                 rb.velocity = Vector2.up * jumpForce;
-
                 jumpTimer += Time.deltaTime;
-            }  else {
+            }
+            else
+            {
                 isJumping = false;
+                animator.SetBool("IsJumping", false); // Imposta il parametro booleano "IsJumping" su false quando il salto è finito
             }
         }
 
-        if (Input.GetButtonUp("Jump")) {
+        if (Input.GetButtonUp("Jump"))
+        {
             isJumping = false;
             jumpTimer = 0;
+            animator.SetBool("IsJumping", false); // Imposta il parametro booleano "IsJumping" su false quando il salto è interrotto
         }
     }
 }
